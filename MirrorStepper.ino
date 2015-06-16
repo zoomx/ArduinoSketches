809e7f8f15a25d1604fa2d44cf481830f46e7452
zoomx <zoomx@tiscalinet.it>
2015-06-16 14:16:51 +0200

    Update MirrorStepper.ino
    First exaple to drive an EasyDriver board

--- MirrorStepper.ino
+++ MirrorStepper.ino
@@ -1,1 +1,21 @@
+/*
+Stepper01
+http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
+
+*/
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Stepper01");
+  pinMode(8, OUTPUT);     
+  pinMode(9, OUTPUT);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}
+
+void loop() {
+  digitalWrite(9, HIGH);
+  delay(1);          
+  digitalWrite(9, LOW); 
+  delay(1);          
+}
 
